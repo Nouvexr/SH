193c6dee5fda8e2b4cd5051d191e25338d6d9c1c
zHeadCrusher <67909733+zHeadCrusher@users.noreply.github.com>
2020-08-06 13:49:53 -0300

    Create teste.sh

---
+++ teste.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# <-- isso é um comentario
+
+echo  "com isso, a mensagem sera exibida no terminal"  #echo
+date #comando para mostrar data e hora"
+
+echo "quem sou eu"
+whoami #comando para mostrar qual é o usuario corrente
+
+echo "qual é o meu sistema"
+uname -s -r #comando para mostrar informaçoes do sistema
+
+echo "Entre com o endereço ip"
+read ip #ele lê o que o usuario escrever no terminal
+echo "varrendo o endereço $ip"
