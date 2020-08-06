49a73e0643d874a662282508a9a26d67f9f56148
zHeadCrusher <67909733+zHeadCrusher@users.noreply.github.com>
2020-08-06 13:45:32 -0300

    Create condiçao.sh

---
+++ condiçao.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "$1" == "" ]
+then
+echo "AULA DO CURSO DE PENTEST PROFISSIONAL"
+echo "0 $0 deve ser passado com argumento: 192.168.2."
+fi
+#é interessante usar isso quando tiver que explicar como um script funciona, e depois marcar 
+#quais opçoes devem estar presentes após o comando de execuçao do script
